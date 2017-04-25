737e5e99a2d48c1f834c41ee85af5d553f6f8d00
simongk <simongk95@gmail.com>
2017-04-25 14:19:45 +0200

    sample working example

--- src/main/java/com/simongk/kolokwium/Application.java
+++ src/main/java/com/simongk/kolokwium/Application.java
@@ -1,13 +1,16 @@
 package com.simongk.kolokwium;
 
+import com.simongk.kolokwium.point.Point;
+import com.simongk.kolokwium.square.Square;
+
 /**
  * Created by Groobaz on 2017-04-25.
  */
 public class Application {
 
     public static void main(String[] args) {
-
+        Draw draw = new Draw();
+        draw.draw(new Square().getPointsOfSquare(10, new Point(5,5)));
     }
 
-    //dlugosc boku, punkt zaczepienia, kwadrat, okrag
 }
